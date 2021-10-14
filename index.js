3890d526d8ba36eef1361876b6f57924dc99ad30
Joyce Huang <65410999+alm0ndjjoy@users.noreply.github.com>
2021-10-14 17:41:26 -0400

    commit tree 16280235ee62f0b01bb48ce06a3047d547d676e1

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+let record = [
+    {
+        year : 1968,
+        result: "N/A"
+    },
+    {
+        year : 1969,
+        result: "W"
+    },
+    {
+        year :1970,
+        result: "N/A"
+
+    }
+]
+function superbowlWin(newArray){
+    let theWin = newArray.find(object => object.result === "W")
+    if (theWin === undefined){
+        return undefined
+    }
+    return theWin.year
+}
+
+superbowlWin(newArray)
+
 
